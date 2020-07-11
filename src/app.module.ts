040d6d1103e59d52e9ad3c44436731b51e586a9f
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-07-11 01:07:45 -0300

    Passando as configurações de acesso para o .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,19 @@ import { ProdutosController } from './produtos/produtos.controller';
 import { ProdutosService } from './produtos/produtos.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Produto } from './produtos/produto.model';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     SequelizeModule.forRoot({
       dialect: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'alura',
-      password: 'admin123',
-      database: 'alura_produtos01',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       models: [Produto],
     }),
     SequelizeModule.forFeature([Produto]),
