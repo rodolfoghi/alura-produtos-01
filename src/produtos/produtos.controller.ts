e26488c8e8f6b22efc8788f5130c0ebd60f1f326
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-07-10 23:34:23 -0300

    Modelando um Produto

--- src/produtos/produtos.controller.ts
+++ src/produtos/produtos.controller.ts
@@ -1,33 +1,34 @@
 import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { Produto } from './produto.model';
 
 @Controller('produtos')
 export class ProdutosController {
-    private produtos: any[] = [];
+    private produtos: Produto[] = [];
 
     constructor() {
-        this.produtos.push({ id: 1, codigo: 'CNTAZ', nome: 'Caneta Azul', perco: 1.99 });
-        this.produtos.push({ id: 2, codigo: 'CNTPR', nome: 'Caneta Preta', perco: 2.99 });
+        this.produtos.push({ id: 1, codigo: 'CNTAZ', nome: 'Caneta Azul', preco: 1.99 });
+        this.produtos.push({ id: 2, codigo: 'CNTPR', nome: 'Caneta Preta', preco: 2.99 });
     }
 
     @Get()
-    async buscaTodos(): Promise<any[]> {
+    async buscaTodos(): Promise<Produto[]> {
         return this.produtos;
     }
 
     @Get(':id')
-    async buscaUm(@Param('id') id: number): Promise<any> {
+    async buscaUm(@Param('id') id: number): Promise<Produto> {
         return this.produtos.filter(x => x.id == id)[0];
     }
 
     @Post()
-    async cria(@Body() produto): Promise<any> {
+    async cria(@Body() produto: Produto): Promise<Produto> {
         produto.id = this.produtos.length + 1;
         this.produtos.push(produto);
         return produto;
     }
 
     @Put(':id')
-    async atualiza(@Param('id') id: number, @Body() produto) {
+    async atualiza(@Param('id') id: number, @Body() produto: Produto) {
         const produtoExistente = this.produtos.find(e => e.id == id);
         const index: number =  this.produtos.indexOf(produtoExistente);
         this.produtos[index] = produto;
