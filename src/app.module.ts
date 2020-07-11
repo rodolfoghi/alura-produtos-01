ab5b196ca35f4778b8195d331f8eac37a12aa71e
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-07-11 00:50:27 -0300

    Integrando com o Sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProdutosController } from './produtos/produtos.controller';
 import { ProdutosService } from './produtos/produtos.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Produto } from './produtos/produto.model';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'alura',
+      password: 'admin123',
+      database: 'alura_produtos01',
+      models: [Produto],
+    }),
+    SequelizeModule.forFeature([Produto]),
+  ],
   controllers: [AppController, ProdutosController],
   providers: [AppService, ProdutosService],
 })
