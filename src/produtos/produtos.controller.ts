72cc2638b414e06f61add6e7734c89804ca460ab
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-07-10 23:56:29 -0300

    Criando o ProdutosService

--- src/produtos/produtos.controller.ts
+++ src/produtos/produtos.controller.ts
@@ -1,41 +1,35 @@
 import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { ProdutosService } from './produtos.service';
 import { Produto } from './produto.model';
 
 @Controller('produtos')
 export class ProdutosController {
-    private produtos: Produto[] = [];
 
-    constructor() {
-        this.produtos.push({ id: 1, codigo: 'CNTAZ', nome: 'Caneta Azul', preco: 1.99 });
-        this.produtos.push({ id: 2, codigo: 'CNTPR', nome: 'Caneta Preta', preco: 2.99 });
+    constructor(private produtosService: ProdutosService) {
     }
 
     @Get()
     async buscaTodos(): Promise<Produto[]> {
-        return this.produtos;
+        return this.produtosService.buscaTodos();
     }
 
     @Get(':id')
     async buscaUm(@Param('id') id: number): Promise<Produto> {
-        return this.produtos.filter(x => x.id == id)[0];
+        return this.produtosService.buscaUm(id);
     }
 
     @Post()
     async cria(@Body() produto: Produto): Promise<Produto> {
-        produto.id = this.produtos.length + 1;
-        this.produtos.push(produto);
-        return produto;
+        return this.produtosService.cria(produto);
     }
 
     @Put(':id')
     async atualiza(@Param('id') id: number, @Body() produto: Produto) {
-        const produtoExistente = this.produtos.find(e => e.id == id);
-        const index: number =  this.produtos.indexOf(produtoExistente);
-        this.produtos[index] = produto;
+        this.produtosService.atualiza(id, produto);
     }
 
     @Delete(':id')
     async remove(@Param('id') id: number) {
-        this.produtos = this.produtos.filter(x => x.id != id);
+        this.produtosService.remove(id);
     }
 }
